0f3a518967fb324a524f2938d017ca75ba442d63
naveennvrgup <naveennvrgup@gmail.com>
2018-12-29 14:56:31 +0530

    added legal partner

--- ui/dist/admin.522ed690.js
+++ ui/dist/admin.522ed690.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54554" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61311" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
