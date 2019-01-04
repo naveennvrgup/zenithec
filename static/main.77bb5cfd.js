afeb32b038eb9a2b88149cb6deeeb421752ce1ac
naveennvrgup <naveennvrgup@gmail.com>
2019-01-04 12:11:41 +0530

    seo stuff

--- static/main.77bb5cfd.js
+++ static/main.77bb5cfd.js
@@ -213,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54048" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51144" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
