92d98762095dcf1214c651f5402951b79edb7b28
naveennvrgup <naveennvrgup@gmail.com>
2019-03-25 12:00:43 +0530

    wildcard

--- main/settings.py
+++ main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'zenithec'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokupp.com','zenithec.appspot.com','naveennvrgup.ml','167.99.255.190','zenithec.com']
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
